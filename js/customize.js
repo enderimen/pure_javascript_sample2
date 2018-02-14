3644d84963988d6c7481f51a72cc50f1c1462ac6
Ender İMEN <enderimen@hotmail.com>
2018-02-14 23:32:00 +0300

    Clock designed.

---
+++ js/customize.js
@@ -0,0 +1,30 @@
+
+
+function setDate() {
+
+	const secondClass = document.querySelector('.second');
+	const minClass = document.querySelector('.min');
+	const hourClass = document.querySelector('.hour');
+	
+	const now = new Date();
+	
+	const secondsNow = now.getSeconds();
+	const secondsDegrees = ((secondsNow / 60) * 360) + 90;
+	
+	secondClass.style.transform = `rotate(${secondsDegrees}deg)`;
+	secondClass.innerHTML = "Second ( "+ secondsNow +" )";
+
+	const minute = now.getMinutes();
+	const minuteDegrees = ((minute / 60) * 360) + 90;
+
+	minClass.style.transform = `rotate(${minuteDegrees}deg)`;
+	minClass.innerHTML = "Minute ( "+ minute +" )";
+
+	const hour = now.getHours();
+	const hourDegrees = ((hour / 24) * 360) + 90;
+
+	hourClass.style.transform = `rotate(${hourDegrees}deg)`;
+	hourClass.innerHTML = "Hour ( "+ hour +" )";
+}
+
+setInterval(setDate,1000);
